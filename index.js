3b3f7d538b85e41cdbed8d0ceacd533f96e89ab5
Lawyn <iustinftw@gmail.com>
2022-10-27 22:23:58 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const eventEmiter = require("events");
 
+class Test extends eventEmiter {
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.emit("welcome", 55);
+    }, 1500);
+  }
+}
+
+let test = new Test();
+
+test.on("welcome", (nr) => {
+  console.log(nr);
+});
